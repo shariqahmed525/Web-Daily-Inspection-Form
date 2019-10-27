bcf22f2b9108458e83dd4ed919b715cd15eaa187
Shariq Ahmed <shariq.ahmed525@gmail.com>
2019-10-27 10:29:22 +0500

    configure app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  getUsers,
+  getAllForms,
+} from "./redux/actions/actions";
+import { store } from "./redux/store/store";
+
+export default App = () => {
+
+  useEffect(() => {
+    store.dispatch(getUsers());
+    store.dispatch(getAllForms());
+  }, []);
+
+  return <Routes />;
 }
-
-export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import Home from '../screens/home/Home';
 
 const history = createBrowserHistory();
 
